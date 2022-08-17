f92e739bd892d09969d9dbc9a5e38732f1b3dde7
Jaewon Kwak <sovioletta@gmail.com>
2022-08-17 01:46:56 -0700

    added main script

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+import utils
+import time
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+# Settings
+username = ''
+line_channel_access_token = ''
+line_receiver_uid = ''
+
+dump_file = './event_history'
+base_date = (datetime.today() - timedelta(days=7)).strftime('%Y-%m-%d')
+en_home_url = 'https://www.eventernote.com'
+en_user_url = '{}/users/{}'.format(en_home_url, username)
+
+pushed_list = utils.load_pushed_list(dump_file)
+push_queue = []
+
+page = requests.get(en_user_url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+actors_list = soup.find('ul', attrs={'class': 'gb_actors_list'}).find_all('li')
+for actor in actors_list:
+    actor_postfix = actor.find('a')['href']
+    en_actor_url = '{}{}/events'.format(en_home_url, actor_postfix)
+
+    page = requests.get(en_actor_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # TODO: traverse pages
+    events_list = soup.find('div', attrs={'class': 'gb_event_list'}).find('ul').find_all('li', recursive=False)
+    for event_soup in events_list:
+        event = utils.event_soup_to_json(event_soup)
+        if event['id'] not in pushed_list and event['date'] >= base_date:
+            print(event)
+            push_queue.append(event)
+            pushed_list[event['id']] = event['date']
+
+    time.sleep(1)
+
+line_bot_api = LineBotApi(line_channel_access_token)
+for event in push_queue:
+    message = '[{date}]\n{title}\n{time}\n{actors}\n{place}\n{url}'.format(title=event['name'],
+                                                                           date=event['date'],
+                                                                           time=event['time_str'],
+                                                                           place=event['place'],
+                                                                           actors=', '.join(event['actors']),
+                                                                           url=event['url'])
+    try:
+        line_bot_api.push_message(line_receiver_uid, TextSendMessage(text=message))
+        pushed_list[event['id']] = event['date']
+    except LineBotApiError as e:
+        # TODO: exception handling
+        pass
+
+# cleanup pushed_list if date is expired (2 weeks)
+cleanup_list = []
+for event_id in pushed_list:
+    event_date = pushed_list[event_id]
+    if event_date < base_date:
+        cleanup_list.append(event_id)
+
+for event_id in cleanup_list:
+    pushed_list.pop(event_id)
+
+utils.dump_pushed_list(dump_file, pushed_list)
---
+++ utils.py
@@ -0,0 +1,44 @@
+import pickle
+
+
+def event_soup_to_json(event_soup):
+    event_name = event_soup.find('div', attrs={'class': 'event'}).find('h4').text.strip()
+    event_id = event_soup.find('div', attrs={'class': 'event'}).find('h4').find('a')['href'].split('/')[-1]
+    if len(event_soup.find_all('div', attrs={'class': 'place'})) == 2:
+        event_place = event_soup.find_all('div', attrs={'class': 'place'})[0].find('a').text.strip()
+    else:
+        event_place = None
+    event_date = event_soup.find('div', attrs={'class': 'date'}).text.strip().split()[0]
+    event_time = event_soup.find_all('div', attrs={'class': 'place'})[-1].text.strip()
+    event_actors = []
+    actors_list_inner = event_soup.find('div', attrs={'class': 'actor'}).find_all('li')[1:]
+    for actor_inner in actors_list_inner:
+        event_actors.append(actor_inner.text.strip())
+
+    event_json = {
+        'name': event_name,
+        'id': event_id,
+        'url': 'https://www.eventernote.com/events/{}'.format(event_id),
+        'place': event_place,
+        'date': event_date,
+        'time_str': event_time,
+        'actors': event_actors
+    }
+
+    return event_json
+
+
+def load_pushed_list(dump_file):
+    pushed_list = dict()  # {events_id: date}
+    try:
+        with open(dump_file, 'rb') as fr:
+            pushed_list = pickle.load(fr)
+    except FileNotFoundError as e:
+        pass
+
+    return pushed_list
+
+
+def dump_pushed_list(dump_file, pushed_list):
+    with open(dump_file, 'wb') as fw:
+        pickle.dump(pushed_list, fw)
